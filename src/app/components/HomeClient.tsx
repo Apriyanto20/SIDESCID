95d78eea311561e3a90247742febd4bdaf7dd4db
AdiApriyanto <adiapriyanto935@gmail.com>
2025-08-07 21:02:16 +0700

    Repair

--- src/app/components/HomeClient.tsx
+++ src/app/components/HomeClient.tsx
@@ -37,11 +37,11 @@ export default function HomePage() {
             <section className="bg-white pt-16 pb-24 px-4" data-aos="zoom-in">
                 <div className="text-center max-w-4xl mx-auto mb-16">
                     <span className="inline-block border border-black rounded-full px-4 py-1 text-xs font-semibold tracking-wide uppercase mb-4 text-black">
-                        Rumah Dataku
+                        Sistem Informasi Desa Cidugaleun Digital
                     </span>
                     <h1 className="text-4xl md:text-5xl font-semibold leading-tight text-black">
-                        Take Community Empowerment <br className="hidden md:block" />
-                        To The Next Level
+                        Sekilas Info <br className="hidden md:block" />
+                        Desa Cidugaleun
                     </h1>
                 </div>
 
