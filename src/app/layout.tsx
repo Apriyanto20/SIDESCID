dda7c6ec30e430647a8475ee04dee8209f833afa
AdiApriyanto <adiapriyanto935@gmail.com>
2025-08-15 06:20:48 +0700

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import { Poppins } from 'next/font/google';
 import Navbar from './components/navbar';
 import Footer from './components/Footer';
+import { Analytics } from "@vercel/analytics/next"
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={poppins.className}>
         <Navbar />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
